38923ac53e87c69989ef2610dce950303b343c82
user1gh1 <125306237+user1gh1@users.noreply.github.com>
2023-03-21 15:15:12 +0400

    set get update

--- classhumansort.cpp
+++ classhumansort.cpp
@@ -3,33 +3,46 @@
 
 class human
 {
-public:
     std::string name;
     int age;
+	public:
+ 	human()
+	{
+		name = "";
+		age = 0;
+	}
+	human(int _age , std::string _name)
+	{
+		age = _age;
+		name = _name;
+	}
+	int get_age()
+	{
+		return age;
+	}
+	std::string get_name()
+	{
+		return name;
+	}
 };
 
 void bubblesort(human* arr,int n);
 
 int main() 
 {
-    human anna,albert,tigran,samvel,john;
-    anna.age = 31;
-    albert.age = 23;
-    tigran.age = 33;
-    samvel.age = 41;
-    john.age = 72;
-    anna.name = "Anna";
-    albert.name = "Albert";
-    tigran.name = "Tigran";
-    samvel.name = "Samvel";
-    john.name = "John";
+    human anna (28, "Anna");
+    human albert ( 23, "Albert" );
+    human tigran (33, "Tigran");
+    human samvel (41, "Samvel");  
+    human john (72, "John");
+
     int SIZE = 5;
     human arr[SIZE] = {anna,john,tigran,samvel,albert};
     bubblesort(arr, SIZE);    
     //show
     for (int i = 0; i < 5 ; i++)
-		std::cout << arr[i].name<< " ";
-    
+		std::cout << arr[i].get_name() << " ";
+    std::cout<<std::endl;    
     return 0;
 }
 
@@ -40,7 +53,7 @@ void bubblesort(human* arr,const int SIZE)
 	{
 		for (int j = 0; j < SIZE-1; j++)
 		{
-			if (arr[j].age > arr[j+1].age)
+			if (arr[j].get_age() > arr[j+1].get_age())
 			{   
 				tmp = arr[j];
 				arr[j] = arr[j+1];
