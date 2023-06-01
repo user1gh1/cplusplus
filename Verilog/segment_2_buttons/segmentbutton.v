89a775c5b84fb0fc067f067e2ea1483e306455e7
Albert <user1gm1@gmail.com>
2023-06-01 15:01:26 +0400

    -

--- Verilog/segment_2_buttons/segmentbutton.v
+++ Verilog/segment_2_buttons/segmentbutton.v
@@ -26,30 +26,29 @@ module segment_display (
   always @(posedge clk) begin
     if (rst) begin
       number <= 4'b0000;
-      segment <= 7'b0000000;
+      segment <= 7'b1111111;
     end
     else begin
-      if (button_plus && !button_minus && number != 4'b1001)
-        number <= number + 1;
-      else if (button_minus && !button_plus && number != 4'b0000)
-        number <= number - 1;
+      if (b_plus_out) //&& number != 4'b1001)
+        number <= number + 4'b0001;
+      else if (b_minus_out) //&& number != 4'b0000)
+        number <= number - 4'b0001;
     end
   end
 
   always @(number) begin
     case(number)
-      4'b0000: segment <= 7'b1000000;
-      4'b0001: segment <= 7'b1111001;
-      4'b0010: segment <= 7'b0100100;
-      4'b0011: segment <= 7'b0110000;
-      4'b0100: segment <= 7'b0011001;
-      4'b0101: segment <= 7'b0010010;
-      4'b0110: segment <= 7'b0000010;
-      4'b0111: segment <= 7'b1111000;
+      4'b0000: segment <= 7'b0000001;
+      4'b0001: segment <= 7'b1001111;
+      4'b0010: segment <= 7'b0010010;
+      4'b0011: segment <= 7'b0000110;
+      4'b0100: segment <= 7'b1001100;
+      4'b0101: segment <= 7'b0100100;
+      4'b0110: segment <= 7'b0100000;
+      4'b0111: segment <= 7'b0001111;
       4'b1000: segment <= 7'b0000000;
-      4'b1001: segment <= 7'b0011000;
-      default: segment <= 7'b0000000;
+      4'b1001: segment <= 7'b0000100;
+      default: segment <= 7'b1111111;
     endcase
   end
-endmodule 
-
+endmodule
